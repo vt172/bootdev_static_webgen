a4117bd49973f30df07a2279cde702335fa7d463
vt172 <park.philippe@gmail.com>
2025-05-02 04:12:06 +0200

    minor changes

---
+++ src/test_textnode.py
@@ -0,0 +1,16 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def something(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -15,11 +15,11 @@ class TextNode():
         self.url = url
     def __eq__(self,other):
         if not isinstance(other, TextNode):
-        return False
+            return False
         return (
             self.text == other.text and
             self.text_type == other.text_type and
             self.url == other.url
         )
     def __repr__(self):
-        return f"TextNode({self.text},{self.text_type},{self.url})"
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
