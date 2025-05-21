637f08c3f45b74aad042e70a1dd06f1a1de11f14
vt172 <park.philippe@gmail.com>
2025-05-21 20:40:29 +0200

    removed a print

--- src/inline.py
+++ src/inline.py
@@ -190,5 +190,4 @@ def text_to_textnode(text):
 		new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
 		new_nodes = split_nodes_image(new_nodes)
 		new_nodes = split_nodes_link(new_nodes)
-	print(new_nodes)
 	return new_nodes
