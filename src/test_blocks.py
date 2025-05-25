0e0de9ea98f2b69ef3e265f05d3a4f1bc32e1835
vt172 <park.philippe@gmail.com>
2025-05-25 14:54:12 +0200

    added standard test cases for the block_to_blocktype function

--- src/test_blocks.py
+++ src/test_blocks.py
@@ -43,3 +43,43 @@ And another block
 				"And another block"
 			],
 		)
+
+class Test_block_to_blocktype(unittest.TestCase):
+	# Easy cases
+	def test_block_heading(self):
+		block = "# This is a heading\n## Subheading\n###### Heading 6"
+		results = block_to_blocktype(block)
+		expectations = BlockType.HEADING
+		self.assertEqual(results,expectations)
+	def test_block_code(self):
+		block = "```Code block```"
+		results = block_to_blocktype(block)
+		expectations = BlockType.CODE
+		self.assertEqual(results,expectations)
+	def test_block_quote(self):
+		block = "> Code\n> Quote"
+		results = block_to_blocktype(block)
+		expectations = BlockType.QUOTE
+		self.assertEqual(results,expectations)
+	def test_block_ulist(self):
+		block = "- test code\n- test code"
+		results = block_to_blocktype(block)
+		expectations = BlockType.ULIST
+		self.assertEqual(results,expectations)
+	def test_block_olist(self):
+		block = "1. test\n2. test\n3. test"
+		results = block_to_blocktype(block)
+		expectations = BlockType.OLIST
+		self.assertEqual(results,expectations)
+	def test_block_paragraph(self):
+		block = "Just a normal text paragraph"
+		results = block_to_blocktype(block)
+		expectations = BlockType.PARAGRAPH
+		self.assertEqual(results,expectations)
+
+	# Edge cases
+	def test_leadingspace_heading(self):
+		block = " # This is a heading"
+		results = block_to_blocktype(block)
+		expectations = BlockType.HEADING
+		self.assertEqual(results,expectations)
