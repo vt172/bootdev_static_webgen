d1ad614968f2f03d27a7ff46d35d451b70655a6f
vt172 <park.philippe@gmail.com>
2025-06-03 19:41:18 +0200

    copy_static(): completed the inner function delete_public()

--- src/main.py
+++ src/main.py
@@ -1,11 +1,33 @@
 from textnode import *
 from htmlnode import *
+from blocks import *
+import sys, shutil, os
+from pathlib import Path
+
+def copy_static():
+    current_path = Path.cwd()
+    static_path = current_path / "static"
+    public_path = current_path / "public"
+
+    print(f"==== {public_path}")
+    
+    def delete_public(path):
+        if not path.exists():
+            print("Please create a public folder at the root of the project")
+        for item in path.iterdir():
+            if item.is_file():
+                print(f"Deleting file : {item}")
+                item.unlink()
+            if item.is_dir():
+                print(f"Deleting folder : {item}")
+                shutil.rmtree(item)
+        return
+
+    delete_public(public_path)
 
 
 def main():
-    result = TextNode("This is some anchor text", "link", "https://google.com")
-    print(f"...{result}")
-    print(f"...{text_node_to_html_node(result)}")
+    copy_static()
 
 
 if __name__ == "__main__":
